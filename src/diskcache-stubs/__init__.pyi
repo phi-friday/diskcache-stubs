99a44613647ecfd4cc3af0044c686ba25cbc8cd3
phi <phi.friday@gmail.com>
2024-03-16 00:36:51 +0900

    fix: ruff error

--- src/diskcache-stubs/__init__.pyi
+++ src/diskcache-stubs/__init__.pyi
@@ -1,26 +1,23 @@
-from .core import (
-    Cache as Cache,
-    DEFAULT_SETTINGS as DEFAULT_SETTINGS,
-    Disk as Disk,
-    ENOVAL as ENOVAL,
-    EVICTION_POLICY as EVICTION_POLICY,
-    EmptyDirWarning as EmptyDirWarning,
-    JSONDisk as JSONDisk,
-    Timeout as Timeout,
-    UNKNOWN as UNKNOWN,
-    UnknownFileWarning as UnknownFileWarning,
-)
+from .core import DEFAULT_SETTINGS as DEFAULT_SETTINGS
+from .core import ENOVAL as ENOVAL
+from .core import EVICTION_POLICY as EVICTION_POLICY
+from .core import UNKNOWN as UNKNOWN
+from .core import Cache as Cache
+from .core import Disk as Disk
+from .core import EmptyDirWarning as EmptyDirWarning
+from .core import JSONDisk as JSONDisk
+from .core import Timeout as Timeout
+from .core import UnknownFileWarning as UnknownFileWarning
 from .fanout import FanoutCache as FanoutCache
-from .persistent import Deque as Deque, Index as Index
-from .recipes import (
-    Averager as Averager,
-    BoundedSemaphore as BoundedSemaphore,
-    Lock as Lock,
-    RLock as RLock,
-    barrier as barrier,
-    memoize_stampede as memoize_stampede,
-    throttle as throttle,
-)
+from .persistent import Deque as Deque
+from .persistent import Index as Index
+from .recipes import Averager as Averager
+from .recipes import BoundedSemaphore as BoundedSemaphore
+from .recipes import Lock as Lock
+from .recipes import RLock as RLock
+from .recipes import barrier as barrier
+from .recipes import memoize_stampede as memoize_stampede
+from .recipes import throttle as throttle
 
 __all__ = [
     "Averager",
